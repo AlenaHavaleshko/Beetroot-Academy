193451005e5a837deb6398a6b7937b128d0257cb
Alena Havaleshko <alena.havaleshko@gmail.com>
2023-04-19 21:23:38 +0300

    added burger

--- lesson-24-project/scripts/index.js
+++ lesson-24-project/scripts/index.js
@@ -2,6 +2,8 @@
  const burgerButton = document.querySelector(".js-hamburger");
 
  burgerButton.addEventListener("click", () => {
+   const containerMain = document.getElementsByClassName("container-main__nav")[0];
+   containerMain.classList.toggle('is-active');
    burgerButton.classList.toggle("is-active");
  });
 })();
