aa2ee630de6928eed9a0313f9322dd0d9a4d0aa1
Alena Havaleshko <alena.havaleshko@gmail.com>
2023-05-29 18:23:42 +0300

    change arrow container__block2-list__arrow,  slidesPerView: 3

--- lesson-40-project-js/script/index.js
+++ lesson-40-project-js/script/index.js
@@ -1,8 +1,47 @@
+
+function moveTo(type) {
+ console.log(type);
+
+ if(type === 'about') {
+  const hdr = document.getElementsByClassName("container__header__hdr align");
+  console.log(hdr);
+  hdr[0].scrollIntoView({behavior: "smooth"});
+ }
+
+ if(type === 'projects') {
+  const hdr = document.getElementsByClassName("container__block1__hdr");
+  console.log(hdr);
+  hdr[0].scrollIntoView({behavior: "smooth"});
+ }
+
+ if(type === 'news') {
+  const hdr = document.getElementsByClassName("container__block2__hdr");
+  console.log(hdr);
+  hdr[0].scrollIntoView({behavior: "smooth"});
+ }
+
+ if(type === 'contact') {
+  const hdr = document.getElementsByClassName("container__footer");
+  console.log(hdr);
+  hdr[0].scrollIntoView({behavior: "smooth"});
+ }
+
+ if(type === 'arrow') {
+  const hdr = document.getElementsByClassName("container__block1__line");
+  console.log(hdr);
+  hdr[0].scrollIntoView({behavior: "smooth"});
+ }
+ 
+
+}
+
+
+
 const swiper = new Swiper('.swiper', {
  // Optional parameters
  direction: 'horizontal',
  loop: true,
- // slidesPerView: 3,
+ slidesPerView: 3,
  spaceBetween: 30,
  
 
@@ -18,8 +57,13 @@ const swiper = new Swiper('.swiper', {
    prevEl: '.swiper-button-prev',
  },
 
+ autoplay: {
+  delay: 4000, 
+},
+
  // And if we need scrollbar
  // scrollbar: {
  //   el: '.swiper-scrollbar',
  // },
 });
+
