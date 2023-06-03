83f2dc7700a0f25d366eceb254de84fd0ebea314
Alena Havaleshko <alena.havaleshko@gmail.com>
2023-06-03 21:12:06 +0300

    hw react

---
+++ lesson-44-react/src/containers/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+function Blog() {
+ return (
+  <div className="blog">
+    <h2 className="blog-header">Blog</h2>
+    <div className="blog-item1"></div>
+    <div className="blog-item2"></div>
+    <div className="blog-item2"></div>
+    <div className="blog-item2"></div>
+  </div>
+ )
+ }
+ export default Blog;
